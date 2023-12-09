a88cb60554f17e3dc652c346174dcbc83abfa8bc
Lars S <>
2023-12-08 22:40:52 -0800

    Day 9, part 1 solved.

---
+++ 9/part_1.py
@@ -0,0 +1,78 @@
+#!/bin/python3
+
+
+"""
+Dang I lost a lot of time on this one because I tried writing the diff algorithm myself and I was failing
+somewhere. I finally looked up how to diff a list of ints and used numpy.diff which instantly gave me the
+correct answer. With my own version of diff the sample data worked fine but a few of my more complicated
+inputs were not reducing to all zeroes.
+
+I think it has to do with negative numbers and even though I was using abs I was still getting a wrong answer.
+I really wish they have show negative numbers in the sample since they are in the real data.
+
+I have found the AoC rules to be directly misleading on several days now. Oh well on to part 2.
+"""
+import numpy
+
+
+def not_all_zeroes(list_of_ints):
+    """I'm positive there is elegant way to do this in Python."""
+    for value in list_of_ints:
+        if value != 0:
+            return True
+    return False
+
+
+def solve(list_of_values):
+    """Work out the differences until they are all zero, and then
+    work out the next value backwards.
+    """
+    res = [list_of_values]
+    while not_all_zeroes(res[-1]):
+        diff = list(numpy.diff(res[-1]))
+        res.append(diff)
+
+    backwards = res[::-1]
+    for index, arr in enumerate(tuple(backwards[:-1])):
+        arr[-1]
+        next_arr = backwards[index + 1]
+        next_value = arr[-1] + next_arr[-1]
+        next_arr.append(next_value)
+
+    # This works but I didn't add zero to the last list. Idk if that will
+    # effect part 2.
+
+    return backwards[-1][-1]
+
+
+def part_1(filename: str) -> None:
+    with open(filename, "r", encoding="UTF-8") as tmpfile:
+        file = tmpfile.readlines()
+
+    answer = 0
+    for line in file:
+        answer += solve([int(v) for v in line.strip().split(" ")])
+
+    print(answer)
+
+
+part_1("input.txt")
+
+# Answer: 1681758908
+
+
+"""
+# I'm leaving my old diff code here so that I can
+# do a post-mortem on why it failed tomorrow.
+# My answer seems broken so I'm trying numpy
+
+def get_differences(list_of_ints):
+    diff = list(numpy.diff(list_of_ints))
+    return diff
+
+    res = []
+    for i, v in enumerate(list_of_ints[:-1]):
+        res.append(abs(v - list_of_ints[i + 1]))
+    return res
+
+"""
--- score.py
+++ score.py
@@ -2,24 +2,48 @@
 """I bought the Awk book, and when I get further in it, I'm going to use Awk for this
 score!"""
 
-# Expand this script to give me a top three days
-# And a top part 1
-# and a top part 2.
-# I will need a data structure that keeps count of the Days and the parts.
+
+def line_parse(line: str):
+    line_split = line.strip().split(": ")
+    key = line_split[0]
+    part1, part2 = [int(i) for i in line_split[1].split(" ")]
+    return key, part1, part2
 
 
-def line_sum(line: str):
-    return sum(int(i) for i in line.split(" "))
+def sum_dict(scores) -> int:
+    score = 0
+    for v in scores.values():
+        score += sum(v)
+    return score
 
 
-with open("RANKS.txt", "r", encoding="UTF-8") as tmpfile:
-    file = tmpfile.readlines()
+def best_three_days(scores):
+    res = []
+    for k, v in scores.items():
+        res.append([sum(v), k])
 
-score = 0
-for line in file[1:-1]:
-    score += line_sum(line.strip().split(": ")[1])
+    res = sorted(res)
+    for day in res[:3]:
+        print(f"{day[1]}: {day[0]}")
+    return res
 
-print(score)
 
-# Six day score, lower is better,
-# 501150
+def score_ranks(filename):
+    with open(filename, "r", encoding="UTF-8") as tmpfile:
+        file = tmpfile.readlines()
+
+    scores = {}
+    for line in file[1:]:
+        key, part1, part2 = line_parse(line)
+        scores[key] = [part1, part2]
+
+    print(f"Your high score is: {sum_dict(scores)}")
+    print(f"Your best part 1 is: {min(scores.values(), key=lambda v: v[0])[0]}")
+    print(f"Your best part 2 is: {min(scores.values(), key=lambda v: v[1])[1]}")
+    print(f"Your best three days are:")
+    best_three_days(scores)
+
+
+score_ranks("RANKS.txt")
+
+# Day 8 score: 592322
