d6a6079c9a4827a75ab226c67f6692472aeef876
Lars S <>
2024-12-02 16:49:22 -0800

    Moved AoC 2023 to sub dir.

---
+++ 2023/days/16/part_1.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+"""
+
+"""
+
+
+# This is for 2d arrays.
+def is_valid_index_pair(array, x, y):
+    len_rows = len(array)
+    len_cols = len(array[0]) if len_rows else 0
+    return 0 <= x < len_rows and 0 <= y < len_cols
+
+
+def hash(x: int, y: int) -> str:
+    """Modifying this method correctly can solve a lot of problems."""
+    return f"{x}:{y}"
+
+
+def solve():
+    """"""
+    score = 0
+    return score
+
+
+def part_1(filename: str) -> None:
+    with open(filename, "r", encoding="UTF-8") as tmpfile:
+        file = tmpfile.readlines()
+
+    for x, line in enumerate(file):
+        for y, char in enumerate(line):
+            ...
+
+    answer = 0
+    print(answer)
+
+
+part_1("input.txt")
+
+# Answer:
+# Test: 46
